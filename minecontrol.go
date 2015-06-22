f15ea2b616d7193d0ad28b1f7c74e8bcb2870e4d
Josh Proehl <josh@daedalusdreams.com>
2015-06-22 11:48:10 -0700

    proof-of-concept connecting to server, getting result

--- minecontrol.go
+++ minecontrol.go
@@ -5,18 +5,19 @@ import (
   "bufio"
   "io"
   "encoding/binary"
+  "net"
 )
 
 
 // A passthrough type allowing us a customer "parser class"
-type RCONParser struct {
+type RCONClient struct {
   *bufio.Reader
   *bufio.Writer
 }
 
 // Get an instance of our "parser class"
-func NewRCONParser(r io.Reader, w io.Writer) *RCONParser {
-  return &RCONParser{
+func NewRCONClient(r io.Reader, w io.Writer) *RCONClient {
+  return &RCONClient{
     Reader: bufio.NewReader(r),
     Writer: bufio.NewWriter(w),
   }
@@ -53,12 +54,12 @@ Maximum request length: 1460 (giving a max payload length of 1446)
 Code exists in the notchian server to split large responses (>4096 bytes) into multiple smaller packets. However, the code that actually encodes each packet expects a max length of 1248, giving a max response payload length of 1234 bytes.
 */
 type RCONPacket struct {
-  length, reqID, reqType int
-  payload []byte
+  length, reqID, reqType int32
+  payload string
   nullPad [2]byte
 }
 
-func (reader *RCONParser) Decode() (*RCONPacket, error) {
+func (reader *RCONClient) Decode() (*RCONPacket, error) {
   pkt := RCONPacket{}
 
   if err := binary.Read(reader, binary.LittleEndian, &pkt.length); err != nil {
@@ -74,11 +75,11 @@ func (reader *RCONParser) Decode() (*RCONPacket, error) {
   }
 
   // Now we have the details, we'll need to load the length-10 bytes. This is because length includes the 2nd and 3rd fields (ints) and the last 2 null bytes.
-  pkt.payload =  make([]byte, pkt.length)
-  if err := binary.Read(reader, binary.LittleEndian, &pkt.payload); err != nil {
+  bytePayload :=  make([]byte, (pkt.length-10))
+  if err := binary.Read(reader, binary.LittleEndian, &bytePayload); err != nil {
     return &pkt, err
   }
-
+  pkt.payload = string(bytePayload)
 
   // Finally, read the last two bytes to make sure the pad is there. If these are not both NULL something went wrong.
   if err := binary.Read(reader, binary.LittleEndian, &pkt.nullPad); err != nil {
@@ -90,20 +91,66 @@ func (reader *RCONParser) Decode() (*RCONPacket, error) {
 }
 
 // This function assumes that the Packet is complete and correct, and just writes the results out.
-func (writer *RCONParser) Encode(pkt *RCONPacket) (error) {
+func (writer *RCONClient) Encode(pkt *RCONPacket) (error) {
   binary.Write(writer, binary.LittleEndian, pkt.length)
   binary.Write(writer, binary.LittleEndian, pkt.reqID)
   binary.Write(writer, binary.LittleEndian, pkt.reqType)
-  binary.Write(writer, binary.LittleEndian, pkt.payload)
+  binary.Write(writer, binary.LittleEndian, []byte(pkt.payload))
   binary.Write(writer, binary.LittleEndian, pkt.nullPad)
   return writer.Flush()
+}
+
+
+func (p *RCONClient) Build(id int, tp int, payload string) (*RCONPacket) {
+  newPkt := RCONPacket{}
+  newPkt.length = int32(10+len(payload))
+  newPkt.reqID = int32(id)
+  newPkt.reqType = int32(tp)
+  newPkt.payload = payload
+  newPkt.nullPad[0] = 0
+  newPkt.nullPad[1] = 0
+  return &newPkt
 }
 
 
 func main() {
   fmt.Println("Minecontrol running...")
 
-  
+  conn, err := net.Dial("tcp", "127.0.0.1:25575")
+  defer conn.Close()
+  if err != nil {
+      fmt.Println(err)
+      return
+  }
+
+  writer := bufio.NewWriter(conn)
+  reader := bufio.NewReader(conn)
+
+  parser := NewRCONClient(reader,writer)
+
+  openPkt := parser.Build(666, 3, "password")
+  fmt.Println("Sending packet: ", openPkt)
+
+  parser.Encode(openPkt)
+  packet, err := parser.Decode()
+
+  if(err != nil) {
+    fmt.Println("FATAL: ", err)
+  }
+
+  fmt.Println("Auth Result packet was", packet)
+
+  getUserPkt := parser.Build(666, 2, "/list")
+  parser.Encode(getUserPkt)
+  rUserPkt, rUserErr := parser.Decode()
+
+  if(rUserErr != nil) {
+    fmt.Println("FATAL: ", rUserErr)
+  }
+
+  fmt.Println("User result was: ", rUserPkt)
+  fmt.Println(rUserPkt.payload)
+
 }
 
 
