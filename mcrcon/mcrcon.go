cb476ab42a11a632a5700d18a409bca65f13b8f6
Josh Proehl <josh@daedalusdreams.com>
2015-10-08 14:18:31 -0700

    Stub documentation for all methods

--- mcrcon/mcrcon.go
+++ mcrcon/mcrcon.go
@@ -1,42 +1,46 @@
+// MCRCON is package for interacting with a minecraft RCON server. It provides all the necessary tools to set up a connection,
+// execute commands, and recieve responses.
 package mcrcon
 
 import (
 	"bufio"
 	"encoding/binary"
+	"fmt"
 	"math/rand"
 	"net"
 	"time"
 )
 
 /*
-From wiki.vg/Rcon
+MCRCONPacket defines the structure of a single packet either to send to or recieve from the RCON server.
 
-Packet Format
+From wiki.vg/Rcon:
+  Packet Format
 
-Integers are little-endian, in contrast with the Beta protocol.
-Responses are sent back with the same Request ID that you send.
-In the event of an auth failure (i.e. your login is incorrect, or you're trying to send commands without first logging in), request ID will be set to -1
+  Integers are little-endian, in contrast with the Beta protocol.
+  Responses are sent back with the same Request ID that you send.
+  In the event of an auth failure (i.e. your login is incorrect, or you're trying to send commands without first logging in), request ID will be set to -1
 
-Field name  Field Type  Notes
-Length      int         Length of remainder of packet
-Request ID  int         Client-generated ID
-Type        int         3 for login, 2 to run a command, 0 for a multi-packet response
-Payload     byte[]      ASCII text
-2-byte pad  byte, byte  Two null bytes
+  Field name  Field Type  Notes
+  Length      int         Length of remainder of packet
+  Request ID  int         Client-generated ID
+  Type        int         3 for login, 2 to run a command, 0 for a multi-packet response
+  Payload     byte[]      ASCII text
+  2-byte pad  byte, byte  Two null bytes
 
-Packets
+  Packet Types
 
-3: Login
-  Outgoing payload: password.
-  If the server returns a packet with the same request ID, auth was successful (note: packet type is 2, not 3). If you get an request ID of -1, auth failed (wrong password).
-2: Command
-  Outgoing payload should be the command to run, e.g. time set 0
-0: Command response
-  Incoming payload is the output of the command, though many commands return nothing, and there's no way of detecting unknown commands.
-  The output of the command may be split over multiple packets, each containing 4096 bytes (less for the last packet). Each packet contains part of the payload (and the two-byte padding). The last packet sent is the end of the output.
+  3: Login
+    Outgoing payload: password.
+    If the server returns a packet with the same request ID, auth was successful (note: packet type is 2, not 3). If you get an request ID of -1, auth failed (wrong password).
+  2: Command
+    Outgoing payload should be the command to run, e.g. time set 0
+  0: Command response
+    Incoming payload is the output of the command, though many commands return nothing, and there's no way of detecting unknown commands.
+    The output of the command may be split over multiple packets, each containing 4096 bytes (less for the last packet). Each packet contains part of the payload (and the two-byte padding). The last packet sent is the end of the output.
 
-Maximum request length: 1460 (giving a max payload length of 1446)
-Code exists in the notchian server to split large responses (>4096 bytes) into multiple smaller packets. However, the code that actually encodes each packet expects a max length of 1248, giving a max response payload length of 1234 bytes.
+  Maximum request length: 1460 (giving a max payload length of 1446)
+  Code exists in the notchian server to split large responses (>4096 bytes) into multiple smaller packets. However, the code that actually encodes each packet expects a max length of 1248, giving a max response payload length of 1234 bytes.
 */
 type MCRCONPacket struct {
 	length, reqID, reqType int32
@@ -44,17 +48,21 @@ type MCRCONPacket struct {
 	nullPad                [2]byte
 }
 
+// MCRCONClient represents a connection to a single RCON server.
 type MCRCONClient struct {
 	Connected bool
 	conn      net.Conn
 	rw        *MCRCONReaderWriter
 }
 
+// MCRCONReaderWriter is a convenience container to hold both the input and output buffers and let them be passed around easily.
 type MCRCONReaderWriter struct {
 	*bufio.Reader
 	*bufio.Writer
 }
 
+// NewClient takes an address and a password, and attempts to set up a TCP connection to an RCON server at the given address,
+// using the given password.
 func NewClient(addr string, passwd string) *MCRCONClient {
 	nClient := MCRCONClient{}
 	nClient.Connected = false
@@ -89,11 +97,14 @@ func NewClient(addr string, passwd string) *MCRCONClient {
 	return &nClient
 }
 
+// Close terminates RCON connection and sets the connected flag to false.
 func (client *MCRCONClient) Close() {
 	client.Connected = false
 	client.conn.Close()
 }
 
+// Decode reads the RCON object's buffer and returns an MCRCONPacket representing binary data in the buffer.
+// NOTE: TODO: This currently doesn't support multi-packet responses, and would behave unpredictably if one is encountered.
 func (client *MCRCONClient) Decode() (*MCRCONPacket, error) {
 	pkt := MCRCONPacket{}
 
@@ -125,6 +136,7 @@ func (client *MCRCONClient) Decode() (*MCRCONPacket, error) {
 	return &pkt, nil
 }
 
+// Encode takes a MCRCONPacket and writes it into the client object's buffer in the correct binary format.
 // This function assumes that the Packet is complete and correct, and just writes the results out.
 func (client *MCRCONClient) Encode(pkt *MCRCONPacket) error {
 	binary.Write(client.rw, binary.LittleEndian, pkt.length)
@@ -136,6 +148,7 @@ func (client *MCRCONClient) Encode(pkt *MCRCONPacket) error {
 }
 
 func (p *MCRCONClient) Build(id int, tp int, payload string) *MCRCONPacket {
+	// Build constructs an MCRCONPacket from raw information.
 	newPkt := MCRCONPacket{}
 	newPkt.length = int32(10 + len(payload))
 	newPkt.reqID = int32(id)
